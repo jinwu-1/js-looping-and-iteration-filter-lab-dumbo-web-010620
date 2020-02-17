5ffcc43edf951f0500fec33024f0552732cc7b9a
Jin Wu <jinn.wuu@gmail.com>
2020-02-17 16:56:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file
 
-function findingMatching() {
-  
+function findingMatching(array, string) {
+  return array.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase()
+  }
 }
